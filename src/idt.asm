b14ce9a388464f1fa3c7cc42d369487e7725f1bb
wufuheng@gmail.com <wufuheng@gmail.com@00f9a818-7106-c0e9-b0de-b11b393a447e>
2011-03-19 16:37:18 +0000

    seperate IDT table

---
+++ src/idt.asm
@@ -0,0 +1,277 @@
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate;*** from 1 to 20 BIOS INT
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate;*** from 21-32 intel reserved
+.irq0:  mc_descp_gate sel_pmr0code,ClockHandler,    0,DA_386Imc_descp_gate;*** user-defined INT
+.irq1:  mc_descp_gate sel_pmr0code,KeyboardHandler, 0,DA_386Imc_descp_gate;***
+.irq2:  mc_descp_gate sel_pmr0code,HWHandler, 0,DA_386Imc_descp_gate;***
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+.irq14:  mc_descp_gate sel_pmr0code,HWHandler, 0,DA_386Imc_descp_gate;***
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+.91h:  mc_descp_gate sel_pmr0code,JiffiesHandler,  0,DA_386Imc_descp_gate+DA_DPL3
+.92h:  mc_descp_gate sel_pmr0code,GetPidHandler,   0,DA_386Imc_descp_gate+DA_DPL3
+.93h:  mc_descp_gate sel_pmr0code,PrintfHandler,   0,DA_386Imc_descp_gate+DA_DPL3
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+mc_descp_gate sel_pmr0code,SpuriousHandler, 0,DA_386Imc_descp_gate
+
+; BunnyOS 1.0
+;
+; Copyright (C) 2011 Wu Fuheng.
+;
+; BunnyOS is free software;  you can  redistribute it and/or modify it under
+; the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by the
+; Free Software Foundation; either version 2.1, or (at your option)  any
+; later version.
+; 
+; BunnyOS is distributed in the hope that it will be useful, but WITHOUT ANY
+; WARRANTY; without  even  the  implied  warranty  of MERCHANTABILITY or
+; FITNESS FOR A PARTICULAR PURPOSE.  See the  GNU General Public License
+; for more details.
+; 
+; You  should  have  received  a  copy of the GNU General Public License
+; along  with  BunnyOS;  see the  file COPYING.  If not, please write to the
+; Free Software Foundation,  51 Franklin Street, Fifth Floor, Boston, MA
+; 02110-1301, USA.
+;%include "h_macro.asm"
+;Interrupt Vector Table 256 items
